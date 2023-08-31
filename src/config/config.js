d2e7341cd95aed4081ece79038e865bf1ff3cfa3
JonathanDevred <devred.jonathan@gmail.com>
2023-08-31 14:58:26 +0200

    packagejson update

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-const frontBackendUrl = 'http://ec2-16-171-43-92.eu-north-1.compute.amazonaws.com'
+const frontBackendUrl = 'https://ec2-16-171-43-92.eu-north-1.compute.amazonaws.com'
   
   
   export default frontBackendUrl;
