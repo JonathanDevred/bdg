61710f6cce75122e84cced645f4e60658945f0c6
JonathanDevred <devred.jonathan@gmail.com>
2023-09-04 17:42:47 +0200

    test heroku

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  port: 3000,
   password: process.env.DB_PASSWORD
 });
 
